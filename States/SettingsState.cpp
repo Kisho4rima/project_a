0ee95dd07d4c1c11c3c2dba41e41bea0054ea3b5
Kisho4rima <amir.fakhim.h@gmail.com>
2023-06-30 09:24:13 +0200

    Zwischenupdate

---
+++ States/SettingsState.cpp
@@ -0,0 +1,5 @@
+//
+// Created by amirfh on 27.06.23.
+//
+
+#include "SettingsState.h"
---
+++ States/SettingsState.h
@@ -0,0 +1,14 @@
+//
+// Created by amirfh on 27.06.23.
+//
+
+#ifndef PROJECT_A_SETTINGSSTATE_H
+#define PROJECT_A_SETTINGSSTATE_H
+
+
+class SettingsState {
+
+};
+
+
+#endif //PROJECT_A_SETTINGSSTATE_H
